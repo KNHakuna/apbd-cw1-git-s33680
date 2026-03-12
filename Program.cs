e96875161a1d2528618de904a5af256e677f91c2
Konrad Niedziółka <s33680@pjwstk.edu.pl>
2026-03-12 13:18:35 +0100

    Added new game method

--- Program.cs
+++ Program.cs
@@ -10,6 +10,34 @@ namespace apbd_cw1
     {
         static void Main(string[] args)
         {
+            NumberGuesser();
+        }
+
+        static void NumberGuesser()
+        {
+            Console.WriteLine("Try to hit a right number: ");
+            int rand = new Random().Next(1, 100);
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (!int.TryParse(input, out int guess))
+                {
+                    Console.WriteLine("That's not a number! Try again: ");
+                }
+                else if (guess == rand)
+                {
+                    Console.WriteLine("You won! Congratulations!!!");
+                    break;
+                }
+                else if (guess < rand)
+                {
+                    Console.WriteLine("Too low! Try again: ");
+                }
+                else
+                {
+                    Console.WriteLine("Too high! Try again: ");
+                }
+            }
         }
     }
 }
