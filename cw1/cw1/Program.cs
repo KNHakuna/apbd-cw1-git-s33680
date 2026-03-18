50c6a986c40b4909bbc070b8f8089c45127bcf4e
Konrad Niedziółka <s33680@pjwstk.edu.pl>
2026-03-18 18:14:16 +0100

    Add GuessTheNumber method, Program class and Main method

--- cw1/cw1/Program.cs
+++ cw1/cw1/Program.cs
@@ -1,3 +1,48 @@
 ﻿class Arcade
 {
+    public void GuessTheNumber()
+    {
+        int n;
+        Random rand = new Random();
+        n = rand.Next(1, 100);
+
+        Console.WriteLine("Guess the number! What is your first guess? ");
+
+        while (true)
+        {
+            string input = Console.ReadLine();
+            int k;
+
+            if (!int.TryParse(input, out k))
+            {
+                Console.WriteLine("That is not a number!");
+                continue;
+            }
+
+            if (k == n)
+            {
+                Console.WriteLine("Congratulations, you won!");
+                break;
+            }
+            else if (k < n)
+            {
+                Console.WriteLine("Too low!");
+            }
+            else
+            {
+                Console.WriteLine("Too high!");
+            }
+
+            Console.WriteLine("\n What is your next guess? ");
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Arcade arcade = new Arcade();
+        arcade.GuessTheNumber();
+    }
 }
